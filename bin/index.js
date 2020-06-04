415be04b5af4b494354959b9e0aeb849d7d468d7
MO <fengzilong1992@gmail.com>
2020-06-04 23:57:59 +0800

    feat: headless by default, use --visible to disable headless

--- bin/index.js
+++ bin/index.js
@@ -5,8 +5,8 @@ const run = require( '../lib' )
 
 cli
   .command( '[...urls]', 'Record videos for these urls' )
-  .option( '--repeat <count>', 'repeat count')
-  .option( '--headless', 'headless')
+  .option( '--repeat <count>', 'Repeat count')
+  .option( '--visible', 'No headless')
   .action( ( urls, options ) => {
     if ( urls.length === 0 ) {
       console.log( 'No url provided' )
@@ -17,7 +17,7 @@ cli
 
     run( urls || [], {
       repeatCount: options.repeat || 3,
-      headless: Boolean( options.headless )
+      headless: Boolean( options.visible ) === false
     } )
   } )
 

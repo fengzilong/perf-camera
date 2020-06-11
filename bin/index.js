619f903d3342743f4db6b8b2e0589fce09818d40
MO <fengzilong1992@gmail.com>
2020-06-11 09:18:57 +0800

    feat: add port option

--- bin/index.js
+++ bin/index.js
@@ -6,6 +6,7 @@ const run = require( '../lib' )
 cli
   .command( '[...urls]', 'Record videos for these urls' )
   .option( '--repeat <count>', 'Repeat count')
+  .option( '--port <port>', 'Use custom port')
   .option( '--visible', 'No headless')
   .action( ( urls, options ) => {
     if ( urls.length === 0 ) {
@@ -17,7 +18,8 @@ cli
 
     run( urls || [], {
       repeatCount: options.repeat || 3,
-      headless: Boolean( options.visible ) === false
+      headless: Boolean( options.visible ) === false,
+      port: options.port ? Number( options.port ) : void 0,
     } )
   } )
 
